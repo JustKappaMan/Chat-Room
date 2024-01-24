1c198dca525d9c7466f9c0a1daaf71879d4983f6
Kirill Volozhanin <volozhaninkirill@gmail.com>
2024-01-24 08:26:16 +0500

    Refactor & improve `logger.py`

--- chatroom/logger.py
+++ chatroom/logger.py
@@ -2,18 +2,42 @@ import logging
 
 
 class Logger:
+    """
+    A class for creating and configuring `logging.Logger` instances.
+    """
+
     __slots__ = ("logger",)
 
-    def __init__(self, name: str):
+    def __init__(
+        self,
+        name: str,
+        level: int = logging.INFO,
+        pattern: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        log_to_file: bool = False,
+        log_to_console: bool = True,
+    ):
+        """
+        Create a new `logging.Logger` and configure it according to the given parameters.
+
+        Use the `get` method to retrieve the instance after initialization.
+        """
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(level)
 
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter(pattern)
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
+        if log_to_file:
+            handler = logging.FileHandler(f"{name}.log", encoding="utf-8")
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
 
-        self.logger.addHandler(stream_handler)
+        if log_to_console:
+            handler = logging.StreamHandler()
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
 
     def get(self) -> logging.Logger:
+        """
+        Retrieve the custom `logging.Logger` instance.
+        """
         return self.logger
